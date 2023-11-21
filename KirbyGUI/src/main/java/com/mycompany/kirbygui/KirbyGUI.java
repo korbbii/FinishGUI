2ec1df587c776c0d308b11952d08376dcf481e4d
korbbii <124796384+korbbii@users.noreply.github.com>
2023-11-21 04:19:51 -0500

    Add files via upload

---
+++ KirbyGUI/src/main/java/com/mycompany/kirbygui/KirbyGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kirbygui;
+
+/**
+ *
+ * @author Edward
+ */
+public class KirbyGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
